316ef97ed9cfa75015fcfcd8a78c2d344854ca3b
garbanza <anzagarb@gmail.com>
2015-11-06 12:37:37 -0600

    release 0.0.2

--- maven-tests/src/garbanza/maventests/AClass.java
+++ maven-tests/src/garbanza/maventests/AClass.java
@@ -1,9 +1,10 @@
+package garbanza.maventests;
 
 public class AClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello maven");
 	}
 
 }
