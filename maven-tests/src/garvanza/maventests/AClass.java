aba1f2406cedf1ba37a2be9b291ea1e101ba5709
garbanza <anzagarb@gmail.com>
2015-11-17 18:38:44 -0600

    hell

--- maven-tests/src/garvanza/maventests/AClass.java
+++ maven-tests/src/garvanza/maventests/AClass.java
@@ -4,7 +4,7 @@ public class AClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello maven");
+		System.out.println("hello "+args[1]);
 	}
 
 }
